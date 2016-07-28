e4230b86605fe629b6c1c4b54ef9732fb9b8bd0a
Merijn Verstraaten <merijn@inconsistent.nl>
2016-07-28 17:01:32 +0200

    Add benchmark running and numerical diffing scripts.

---
+++ numdiff.awk
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+exe=`basename "$0"`
+
+usage () {
+      printf "Usage:\n"
+      printf "$exe file1 file2 [file3...]\n"
+      printf "\nOptions:\n"
+      printf "\t-h | --help\n"
+      printf "\t\tThis help.\n"
+      printf "\t-p PREC | --precision PREC\n"
+      printf "\t\tSize of floating point error to ignore.\n"
+      printf "\t-q | --quiet\n"
+      printf "\t\tOnly print results in case of >0 errors.\n"
+      printf "\t-v | --verbose\n"
+      printf "\t\tPrint invidual errors exceeding threshold.\n"
+      exit
+}
+
+if [ "$#" -lt 2 ]; then
+    usage
+fi
+
+prec=0.0
+args=""
+n=0
+v=1
+while
+  case $1 in
+    -h | --help)
+      usage
+      ;;
+    -p | --precision)
+      [ -z "$2" ] && printf "Missing argument for: $1\n\n" && usage || shift 1
+      prec=$1
+      ;;
+    -v | --verbose)
+      v=2
+      ;;
+    -q | --quiet)
+      v=0
+      ;;
+    --)
+      shift 1
+      args+=" $*"
+      n=$(($n + $#))
+      break
+      ;;
+    "") break
+      ;;
+    -*)
+      printf "Unknown option: $1\n\n"
+      usage
+      ;;
+    *)
+      args+=" $1"
+      n=$(($n + 1))
+      ;;
+  esac
+  shift 1
+do
+    :
+done
+
+paste $args | awk -v args="$args" -v num_args="$n" -v precision="$prec" -v verbose="$v" '
+function abs(v) { return v < 0 ? -v : v }
+BEGIN { count = 0; max_diff=0; split(args,files," ");}
+{
+    off=NF/num_args;
+    for (i = 2; i <= off; i++) {
+        for (j = 1; j < num_args; j++) {
+            k = i + j*off
+            diff = abs($i - $k);
+            if (diff > precision) {
+                if (diff > max_diff) {
+                    max_diff = diff;
+                }
+                count++;
+                if (verbose == 2) {
+                    file_msg = ""
+                    if (num_args > 2) {
+                        file_msg = " (" files[1] " and " files[1+j] ")"
+                    }
+                    print "Line",NR,"field",i file_msg ":"
+                    print $i,$k,"(difference:",diff ")"
+                    print ""
+                }
+            }
+        }
+    }
+}
+END {
+    if (verbose == 2) print""
+    if (verbose || count > 0) print "Error count:",count"\tMax diff:",max_diff
+}
+'
---
+++ run-benchmarks.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+exe=`basename "$0"`
+
+usage () {
+      printf "Options:\n"
+      printf "\t-h | --help\n"
+      printf "\t\tThis help.\n"
+      printf "\t-e EXT | --extension EXT\n"
+      printf "\t\tExtension to use for algorithm output files.\n"
+      printf "\t-n NUM | --count NUM\n"
+      printf "\t\tNumber of times to run algorithm.\n"
+      printf "\t-r DIR | --result-dir DIR\n"
+      printf "\t\tDirectory to write results too.\n"
+      printf "\t-s | --save-output\n"
+      printf "\t\tSave algorithm output.\n"
+      printf "\t-t EXT | --timing-extension EXT\n"
+      printf "\t\tExtension to use for algorithm timing files.\n"
+      exit
+}
+
+outputExtension="out"
+n=30
+resultdir="$HOME/results"
+save=""
+timingsExtension="timings"
+while
+  case $1 in
+    -h | --help)
+      usage
+      ;;
+    -e | --extension)
+      [ -z "$2" ] && printf "Missing argument for: $1\n\n" && usage || shift 1
+      if [ -n "$1" ]; then
+          if [[ $1 == .* ]]; then
+              outputExtension="${1#.}"
+          else
+              outputExtension="$1"
+          fi
+      else
+          printf "Zero length argument not allowed for $1!\n\n"
+          usage
+      fi
+      ;;
+    -n | --count)
+      [ -z "$2" ] && printf "Missing argument for: $1\n\n" && usage || shift 1
+      n=$1
+      ;;
+    -r | --result-dir)
+      [ -z "$2" ] && printf "Missing argument for: $1\n\n" && usage || shift 1
+      resultdir="$1"
+      ;;
+    -s | --save-output)
+      save="1"
+      ;;
+    -t | --timing-extension)
+      [ -z "$2" ] && printf "Missing argument for: $1\n\n" && usage || shift 1
+      if [ -n "$1" ]; then
+          if [[ $1 == .* ]]; then
+              timingsExtension="${1#.}"
+          else
+              timingsExtension="$1"
+          fi
+      else
+          printf "Zero length argument not allowed for $1!\n\n"
+          usage
+      fi
+      ;;
+    --)
+      shift 1
+      args+=" $*"
+      n=$(($n + $#))
+      break
+      ;;
+    "") break
+      ;;
+    -*)
+      printf "Unknown option: $1\n\n"
+      usage
+      ;;
+    *)
+      args+=" $1"
+      n=$(($n + 1))
+      ;;
+  esac
+  shift 1
+do
+    :
+done
+
+mkdir -p "$resultdir"
+
+for gpu in "TitanX"; do
+    run () {
+        prun -np 1 -native "-C $gpu" CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7,8,9,10" $*
+    }
+    for alg in `run ./main list algorithms`; do
+        for impl in `run ./main list implementations -a $alg`; do
+            for path in ../graphs/*.graph; do
+                graph=$(basename $path)
+                graph=${graph%.graph}
+                extra_flags=""
+                if [ -n "$save" ]; then
+                    extra_flags+="-o $resultdir/$graph.$alg.$impl.$gpu.$outputExtension"
+                fi
+                run ./main -a $alg -k $impl -n $n -t $resultdir/$graph.$alg.$impl.$gpu.$timingsExtension $path $extra_flags &
+            done
+        done
+    done
+    wait
+done
+
