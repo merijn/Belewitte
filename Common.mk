530190aed017b3b0be10060a96c4703e36988b58
Merijn Verstraaten <merijn@inconsistent.nl>
2018-08-16 16:21:28 +0200

    Fix bug in recipe for cabal new-build.

--- Common.mk
+++ Common.mk
@@ -102,5 +102,5 @@ $(BUILD)/kernels/:
 .PHONY: haskell-dependencies
 haskell-dependencies:
 	$(PRINTF) " CABAL\t$@\n"
-	$(AT)cabal --builddir="$(realpath $(abspath $(BUILD)/haskell/))" \
+	$(AT)cabal --builddir="$(abspath $(BUILD)/haskell/)" \
 	    new-build all $(if $(AT),2>/dev/null >/dev/null,)
