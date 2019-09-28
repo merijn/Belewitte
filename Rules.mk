280991c36deb2349297ba5fb0efc43c22e0e1b32
Merijn Verstraaten <merijn@inconsistent.nl>
2019-09-28 14:39:23 +0200

    Fix CUDA build rules.

--- Rules.mk
+++ Rules.mk
@@ -61,21 +61,21 @@ $(DEST)/%.tsan.o: $(SRCDIR)/%.cpp | $(DEST)/
 	$(AT)$(CXX) $(CXXFLAGS) -I. $< -c -o $@
 endif
 
-$(DEST)/%.obj: %.cu | $(DEST)/
+$(DEST)/%.obj: $(SRCDIR)/%.cu | $(DEST)/
 	$(PRINTF) " NVCC\t$*.cu\n"
 	$(AT)$(NVCC) $(NVCCXXFLAGS) -M -I. $< -o $(@:.obj=.d)
 	$(AT)$(SED) -i.bak "s#$(notdir $*).o#$(@) $*.ptx#" $(@:.obj=.d)
 	$(AT)rm -f $(@:.obj=.d).bak
 	$(AT)$(NVCC) $(NVCCXXFLAGS) $(NVCCARCHFLAGS) -lineinfo -I. --device-c $< -o $@
 
-$(DEST)/%.debug.obj: %.cu | $(DEST)/
+$(DEST)/%.debug.obj: $(SRCDIR)/%.cu | $(DEST)/
 	$(PRINTF) " NVCC\t$*.cu\n"
 	$(AT)$(NVCC) $(NVCCXXFLAGS) -M -I. $< -o $(@:.obj=.d)
 	$(AT)$(SED) -i.bak "s#$(notdir $*).o#$(@) $*.ptx#" $(@:.obj=.d)
 	$(AT)rm -f $(@:.obj=.d).bak
 	$(AT)$(NVCC) $(NVCCXXFLAGS) $(NVCCARCHFLAGS) --device-debug -I. --device-c $< -o $@
 
-%.ptx: %.cu
+%.ptx: $(SRCDIR)/%.cu
 	$(PRINTF) " PTX\t$*.cu\n"
 	$(AT)$(NVCC) $(NVCCXXFLAGS) -M -I. $< -o $(BUILD)$(@:.ptx=.d)
 	$(AT)$(SED) -i.bak "s#$(notdir $*).o#$(@) $*.ptx#" $(BUILD)$(@:.ptx=.d)
