5bf3861a57f354c7909e1871903e12056e80b662
Merijn Verstraaten <merijn@inconsistent.nl>
2019-10-20 22:23:55 +0200

    Add commands for setting default variant config/platform.

--- benchmark-analysis/ingest-src/Commands/Set.hs
+++ benchmark-analysis/ingest-src/Commands/Set.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Commands.Set (commands) where
 
 import qualified Control.Monad.Catch as Except
+import qualified Data.Text as T
 import System.Exit (exitFailure)
 
 import Core
 import OptionParsers
 import Schema
+import Sql (SqlBackend, SqlRecord, ToBackendKey, (==.), (=.))
 import qualified Sql
 
 data Force = NoForce | Force deriving (Show, Eq)
@@ -24,6 +29,30 @@ commands = CommandGroup CommandInfo
             \runner."
         }
         $ setRunCommand <$> forceOpt <*> strArgument runCmd
+    , CommandGroup CommandInfo
+        { commandName = "default"
+        , commandHeaderDesc = "change registered defaults"
+        , commandDesc = "Change default entries for database tables."
+        }
+        [ SingleCommand CommandInfo
+            { commandName = "platform"
+            , commandHeaderDesc =
+                "change which platform is used for non-timing runs"
+            , commandDesc =
+                "Changes which platform is used for output validation and \
+                \registering properties."
+            }
+            $ setDefault "platform" PlatformIsDefault
+                     <$> forceOpt <*> keyParser
+        , SingleCommand CommandInfo
+            { commandName = "variant-config"
+            , commandHeaderDesc = "change which config is plotted by default"
+            , commandDesc =
+                "Changes which variant is shown by default in plots."
+            }
+            $ setDefault "variant config" VariantConfigIsDefault
+                     <$> forceOpt <*> keyParser
+        ]
     ]
   where
     runCmd :: Mod ArgumentFields a
@@ -32,6 +61,10 @@ commands = CommandGroup CommandInfo
     forceOpt :: Parser Force
     forceOpt = flag NoForce Force $ mconcat
         [ long "force", help "Override pre-existing values when setting." ]
+
+    keyParser :: ToBackendKey SqlBackend v => Parser (Key v)
+    keyParser = argument (toSqlKey <$> auto) $ mconcat
+        [ metavar "ID", help "Id to change." ]
 
 setRunCommand :: Force -> Text -> SqlM ()
 setRunCommand force = setCommand
@@ -49,3 +82,27 @@ setRunCommand force = setCommand
         exitFailure
 
     sqliteException e = Except.throwM e
+
+setDefault
+    :: (SqlRecord r, ToBackendKey SqlBackend r)
+    => String -> EntityField r Bool -> Force -> Key r -> SqlM ()
+setDefault name field force key = do
+    Sql.get key >>= checkExists
+
+    numDefault <- Sql.count [field ==. True]
+    case (numDefault, force) of
+        (0, _) -> Sql.update key [field =. True]
+        (_, Force) -> do
+            Sql.updateWhere [] [field =. False]
+            Sql.update key [field =. True]
+        _ -> liftIO $ do
+            putStrLn $ "Default " <> name <> " is already set!"
+            putStrLn "Use --force to overwrite."
+            exitFailure
+  where
+    checkExists :: Maybe a -> SqlM ()
+    checkExists (Just _) = return ()
+    checkExists Nothing = logErrorN msg >> liftIO exitFailure
+
+    msg :: Text
+    msg = mconcat ["No ", T.pack name, " with id #", showSqlKey key]
