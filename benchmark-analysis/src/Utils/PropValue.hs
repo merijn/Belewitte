fedd9a9f0bee4d63633f96a3a2d1eebf61a6b231
Merijn Verstraaten <merijn@inconsistent.nl>
2020-05-22 17:57:52 +0200

    Add a labelled property value type.

---
+++ benchmark-analysis/src/Utils/PropValue.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE RecordWildCards #-}
+module Utils.PropValue where
+
+import Data.Int (Int64)
+import Foreign.Ptr (castPtr)
+import Foreign.Storable (Storable(..))
+
+data PropValue = PropValue
+    { propValuePropId :: {-# UNPACK #-} !Int64
+    , propValueValue :: {-# UNPACK #-} ! Double
+    } deriving (Show)
+
+instance Storable PropValue where
+    sizeOf _ = sizeOf (0 :: Int64) + sizeOf (0.0 :: Double)
+    alignment _ = alignment (0.0 :: Double)
+    peek ptr = PropValue <$> peek (castPtr ptr)
+                         <*> peekByteOff ptr (sizeOf (0 :: Int64))
+
+    poke ptr PropValue{..} = do
+        poke (castPtr ptr) propValuePropId
+        pokeByteOff ptr (sizeOf (0 :: Int64)) propValueValue
