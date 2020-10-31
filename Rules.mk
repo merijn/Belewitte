396a3a0815f0bf2d354555518e15882a3894ac36
Merijn Verstraaten <merijn@inconsistent.nl>
2020-10-31 15:49:25 +0100

    Fix typo in PTX rules.

--- Rules.mk
+++ Rules.mk
@@ -77,9 +77,9 @@ $(DEST)/%.debug.obj: $(SRCDIR)/%.cu | $(DEST)/
 
 %.ptx: $(SRCDIR)/%.cu
 	$(PRINTF) " PTX\t$*.cu\n"
-	$(AT)$(NVCC) $(NVCCXXFLAGS) -M -I. $< -o $(BUILD)$(@:.ptx=.d)
-	$(AT)$(SED) -i.bak "s#$(notdir $*).o#$(@) $*.ptx#" $(BUILD)$(@:.ptx=.d)
-	$(AT)rm -f $(BUILD)$(@:.ptx=.d).bak
+	$(AT)$(NVCC) $(NVCCXXFLAGS) -M -I. $< -o $(BUILD)/$(@:.ptx=.d)
+	$(AT)$(SED) -i.bak "s#$(notdir $*).o#$(@) $*.ptx#" $(BUILD)/$(@:.ptx=.d)
+	$(AT)rm -f $(BUILD)/$(@:.ptx=.d).bak
 	$(AT)$(NVCC) $(NVCCXXFLAGS) -arch $(PTXARCH) -I. -src-in-ptx --ptx $< -o $@
 	$(AT)$(SED) -i.bak '1,/^\t\/\/ .globl/ d; /^\t.file\t/,$$ d' $@
 	$(AT) rm -f $@.bak
