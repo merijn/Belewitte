c8c803bdaa850cf8f53214bf83ef6a2f0c478dc9
Merijn Verstraaten <merijn@inconsistent.nl>
2021-08-05 17:32:07 +0200

    Fix version reporting for algorithms to ignore Makefile changes.

--- report-kernel-commit.sh
+++ report-kernel-commit.sh
@@ -3,9 +3,9 @@
 cd "$(dirname $0)"
 
 if [ -d ".hg" ]; then
-    COMMIT="$(hg log -r "last(modifies('$1/*'))" -T "{gitnode}\n")"
+    COMMIT="$(hg log -r "last(modifies('set:$1/* - set:$1/Makefile'))" -T "{gitnode}\n")"
 elif [ -d ".git" ]; then
-    COMMIT="$(git log -n 1 --pretty=format:%H $1)"
+    COMMIT="$(git log -n 1 --pretty=format:%H $1 ":!$1/Makefile")"
 fi
 
 if [ -n "$COMMIT" ]; then
